b568258201f0c3be3891417567bf76f8e04e7a81
Andre196908 <163765462+Andre196908@users.noreply.github.com>
2025-07-16 21:17:15 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import EduPilot from './EduPilot';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EduPilot />
-  </React.StrictMode>
-);
