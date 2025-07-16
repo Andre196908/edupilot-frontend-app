4ffe530866ae512f4e5ea65f7122b36045123d44
Andre196908 <163765462+Andre196908@users.noreply.github.com>
2025-07-16 20:12:54 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EduPilot from './EduPilot';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EduPilot />
+  </React.StrictMode>
+);
